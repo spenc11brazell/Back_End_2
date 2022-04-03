878ad3e9c984abd7ba73ce9a56228288df5e5aea
Spencer Brazell <sa.brazell11@gmail.com>
2022-04-02 19:02:23 -0600

    progress update

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const PORT = 4004
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const { getHouse, deleteHouse, createHouse, updateHouse } = require('./controllers/movie.controllers')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/houses', getHouse)
+app.delete('/api/houses/:id', deleteHouse)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
